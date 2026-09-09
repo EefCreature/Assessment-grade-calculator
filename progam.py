9be4abf138757442152539b59b954a000044d368
ethanx300-prog <ethanx300@gmail.com>
2026-09-09 17:20:22 +1200

    v1

---
+++ progam.py
@@ -0,0 +1,14 @@
+
+
+def grade_calculation(grade: int,grade_total: int,waiting: float):
+    print(grade,grade_total,waiting)
+
+    pass
+
+
+
+
+grade_calculation(input("grade: "),input("grade total: "),input("waiting: "))
+
+
+
