aac60f4b642ab2a92c5deba9f687edb7c26130fb
ethanx300-prog <ethanx300@gmail.com>
2026-09-15 15:48:09 +1200

    hs

--- progam.py
+++ progam.py
@@ -1,5 +1,25 @@
+"""
+    Title:
+    Description:
+    Author: Ethan
+
+"""
+
 import guizero as gz
 
+
+    
+grade = {
+        "grade": [],
+        "grade_total": [],
+        "waiting": []
+    }
+
+grade_list = {
+        "grade": [],
+        "grade_total": [],
+        "waiting": []
+    }
 
 grade_naer = {
     "A+": 90,
@@ -16,6 +36,9 @@ grade_naer = {
 }
 
 def coe(vall,type:type):
+    """
+    
+    """
     try:
         type(vall)
     except:
@@ -24,10 +47,16 @@ def coe(vall,type:type):
     pass
 
 def grade_calculation(grade: int,grade_total: int,waiting: float):
+    """
+    
+    """
     grade = int(grade)
     grade_total = int(grade_total)
     waiting = float(waiting)
-    
+
+    if grade > grade_total and grade != grade_total:
+        gz.error(title="error",text=("Error grade is prot sed grade total" ))
+        return 0
     if grade_total != 0:
         fraction_grade = grade / grade_total
     else:
@@ -36,7 +65,9 @@ def grade_calculation(grade: int,grade_total: int,waiting: float):
 
 
 def grade_handling(grade):
+    """
     
+    """
     through_grade = 0
     for i in range(len(grade["grade"])):
         
@@ -56,22 +87,22 @@ def get_grade_naer(grade):
         if grade_naer[i] == resot:
             return(i)
 
+"""
 
-
+"""
+def exas():
+    """
     
-grade = {
-        "grade": [],
-        "grade_total": [],
-        "waiting": []
-    }
+    """
 
-grade_list = {
-        "grade": [],
-        "grade_total": [],
-        "waiting": []
-    }
+    app.destroy()
+    pass
+
 
 def cale():
+    """
+    
+    """
     global grade
    
     
@@ -87,8 +118,10 @@ def cale():
             try :
                 s = float(s)
             except:
-                print(False,gr,s)
+                
+                gz.error(title="error",text=("Error " + str(gr)+": "+ str(s)+" not a namer" ))
                 return
+                
             
             grade[gr].append(s)
 
@@ -98,25 +131,34 @@ def cale():
         if grade["waiting"][i] != 0 :
             grade["waiting"][i] /= 100
         waiting_toll +=grade["waiting"][i]
+
     if waiting_toll != 1:
+        gz.error(title="error",text=("Error wast is not eqill 100" ))
         return
+    grade_text.value = str("grade:" + str( round(grade_handling(grade)[0] * 100)))
+    naer_text.value = str("naer:" + str( grade_handling(grade)[1]))
+    if  grade_handling(grade)[0] * 100 >= 50:
+        past_text.value = "past: yas"
+    else:
+        past_text.value = "past: no"
     
-    print("-"*40)
-    print("Grade:",grade_handling(grade)[1],round(grade_handling(grade)[0] * 100))
+"""
 
-
-
+"""
 app = gz.App("s")
 
 man_box = gz.Box(app,width="fill",border=True,layout="grid")
 inpot_box = gz.Box(man_box,border=True,layout="grid",width="fill",height="fill",grid=[0, 0])
-bot_box = gz.Box(man_box,border=True,layout="grid",width="fill",height=100,grid=[0, 1],align="left")
+apit_box = gz.Box(man_box,border=False,layout="grid",width="fill",height="fill",grid=[0, 1],align="left")
+bot_box = gz.Box(man_box,border=True,layout="grid",width="fill",height=100,grid=[0, 2],align="left",)
 
 gz.Text(inpot_box,text="Marks  ",grid=[1, 0],  size=10)
 gz.Text(inpot_box,text="Marks max",grid=[2, 0],  size=10)
 gz.Text(inpot_box,text="Weight",grid=[3, 0],  size=10)
 
-
+grade_text =gz.Text(apit_box, width="fill", grid=[0, 0], text='grade: ---',align="left") 
+naer_text =gz.Text(apit_box, width="fill", grid=[1, 0], text='naer: --',align="left") 
+past_text = gz.Text(apit_box, width="fill", grid=[2, 0], text='past: ---',align="left")
 
 for i in range(4):
     text = str("Course:" + str(i+1))
@@ -130,7 +172,7 @@ for i in range(4):
 submitPushButton = gz.PushButton(
     bot_box, text="Calculate",  grid=[0, 0], align='left',command=cale, padx=5, pady=5)
 exitPushButton = gz.PushButton(
-    bot_box, text="Exit", grid=[1, 0], align='right', padx=5, pady=5)
+    bot_box, text="Exit", grid=[1, 0], align='right',command=exas, padx=5, pady=5)
 
 app.display()
 
